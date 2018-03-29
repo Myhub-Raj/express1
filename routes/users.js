f1f363fe9d958b66ac7b26a2bd86cb9314af582f
gitworkMy <rajesh.talentrace@gmail.com>
2018-03-29 14:50:53 +0530

    First commit

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+var user_controller = require('../controllers/userController');
+
+router.get('/', user_controller.index);
+
+
+router.post('/', user_controller.user_create);
+
+module.exports = router;
